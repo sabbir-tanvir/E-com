c50b725c2124d12eacd5754a471bfef6bd643042
Sabbir Tanvir <106466798+sabbir-tanvir@users.noreply.github.com>
2025-06-04 19:13:13 +0600

    update delet product

--- backend/controller/productControler.js
+++ backend/controller/productControler.js
@@ -12,6 +12,9 @@ export const createProduct = async(req,res) =>{
     })
 }
 
+
+
+//Get the Product
 export const getAllProducts=async (req,res) => {
     const products = await Product.find();
     res.status(200).json({
@@ -21,7 +24,61 @@ export const getAllProducts=async (req,res) => {
 }
 
 
+//Update the Product 
+export const updateProduct=async(req,res) =>{
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
 
-export const updateProduct=async(req.res) => {
+            success:false,
+            message:"Product is not Found"
+        })
+    }
+    product= await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new: true ,
+        runValidators:true
+    } )
+
+    res.status(200).json({
+        success:true,
+        product
+    })
     
+}
+
+
+//Delete the Product
+
+export const deletProduct = async (req,res) => {
+    let product = await Product.findById(req.params.id);
+        if(!product){
+        return res.status(500).json({
+
+            success:false,
+            message:"Product is not Found"
+        })
+    }
+    product= await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+        success:true,
+        message:"Product deleted successfully"
+    })
+}
+
+
+// accessing single aProduct
+
+export const getSingleProduct = async(req,res)=> {
+    const product = await Product.findById(req.params.id);
+        if(!product){
+        return res.status(500).json({
+
+            success:false,
+            message:"Product is not Found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        product
+    })
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createProduct, getAllProducts } from '../backend/controller/productControler.js';
+import { createProduct, deletProduct, getAllProducts, getSingleProduct, updateProduct } from '../backend/controller/productControler.js';
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts).post(createProduct)
-
+router.route("/products").get(getAllProducts).post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deletProduct).get(getSingleProduct);
 
 export default router;
